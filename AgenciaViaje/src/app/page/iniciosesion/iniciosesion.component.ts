9b744cf37dcbbf9130b570862bd80b45c478548b
Nico <nicolas96vlc@gmail.com>
2024-04-27 15:26:21 +0200

    Se ha instalado los iconos de bootstrap, (añadido el path a angular.json), se ha editado el componente inicio sesion y se ha dejado listo su html y css junto al componente comun navbar.

--- AgenciaViaje/src/app/page/iniciosesion/iniciosesion.component.ts
+++ AgenciaViaje/src/app/page/iniciosesion/iniciosesion.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../component/footer/footer.component';
 
 @Component({
   selector: 'app-iniciosesion',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './iniciosesion.component.html',
   styleUrl: './iniciosesion.component.css'
 })
