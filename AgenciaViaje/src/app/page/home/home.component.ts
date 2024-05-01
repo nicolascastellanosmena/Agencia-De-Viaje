ba99b62c5814746c0670d4e22d9cd69056dac6ad
Marcos <marcosfinkiel24@gmail.com>
2024-05-01 21:10:21 +0200

    Se terminaron las cartas del home con su respectiva api y se corrigieron unos estilos

--- AgenciaViaje/src/app/page/home/home.component.ts
+++ AgenciaViaje/src/app/page/home/home.component.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../component/footer/footer.component';
 import { RouterLink } from '@angular/router';
+import { Destinos } from '../../common/destinos';
+import { DestinosService } from '../../services/destinos.service';
+import { CurrencyPipe, SlicePipe } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent, RouterLink],
+  imports: [FooterComponent, RouterLink, SlicePipe, CurrencyPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  bDestinos:Destinos={continentes:[]};
 
-  
+  constructor(private servicio:DestinosService){}
+
+  ngOnInit(): void {
+    this.load()
+  }
+
+  load(){
+    this.servicio.getDestinos().subscribe(
+      {
+        next: (destino) => {
+          this.bDestinos = destino;
+          console.log(destino);
+           
+        },
+        error: (error) => {
+          console.error('Error al cargar los destinos:', error);
+         
+        },
+        complete: () => {
+          console.log('La carga de destinos se ha completado');
+         
+        }
+      }
+    )
+  }
+
+  buscadorOferta(pais:string): string{
+      return pais;
+  }
 }
