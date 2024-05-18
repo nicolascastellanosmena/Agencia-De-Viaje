ea5d2614f86616d70ca5fdaaeb470880a8b56ccb
Andres Pariche <andresarturo.parichegonzalez@cieep.com>
2024-05-18 20:03:09 +0200

    SE reestructuro la pagina de catalogo, ya no se necesita el componente cards, añadi una img4 a destinos.json y se arreglo el problema de menu al responsivo

--- AgenciaViaje/src/app/common/destinos.ts
+++ AgenciaViaje/src/app/common/destinos.ts
@@ -15,6 +15,7 @@ export interface ImagenesDestacada {
   img1: string
   img2: string
   img3: string
+  img4:string
 }
 
 export interface Destino {
--- AgenciaViaje/src/app/page/catalogo/catalogo.component.ts
+++ AgenciaViaje/src/app/page/catalogo/catalogo.component.ts
@@ -1,15 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CardsComponent } from '../../component/cards/cards.component';
 import { FooterComponent } from '../../component/footer/footer.component';
+import { DestinosService } from '../../services/destinos.service';
+import { Destinos } from '../../common/destinos';
+import { SlicePipe } from '@angular/common';
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [RouterLink,CardsComponent,FooterComponent],
+  imports: [RouterLink,CardsComponent,FooterComponent,SlicePipe],
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.css'
 })
-export class CatalogoComponent {
-  
+export class CatalogoComponent implements OnInit{
 
-}
+  bDestinos:Destinos={continentes:[]}
+
+  constructor(private servicio:DestinosService){}
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load(){
+    this.servicio.getDestinos().subscribe(
+      {
+        next: (destino) => {
+          this.bDestinos = destino;
+           
+        },
+        error: (error) => {
+          console.error('Error al cargar los destinos:', error);
+         
+        },
+        complete: () => {         
+        }
+      }
+    )
+
+
+  }
+
+  }
+
